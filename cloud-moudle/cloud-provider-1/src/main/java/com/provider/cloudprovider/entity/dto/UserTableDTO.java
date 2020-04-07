a12efb22dc7fcaadbb1348e234c048ccb18415f1
起个名太难 <45991386+liuxing5457@users.noreply.github.com>
2020-04-07 10:44:15 +0800

    修改

--- cloud-moudle/cloud-provider-1/src/main/java/com/provider/cloudprovider/entity/dto/UserTableDTO.java
+++ cloud-moudle/cloud-provider-1/src/main/java/com/provider/cloudprovider/entity/dto/UserTableDTO.java
@@ -1,135 +1,74 @@
 package com.provider.cloudprovider.entity.dto;
 
+import lombok.Data;
+
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @Author: carter
  * @Date: 2019/11/6 17:55
  * @Version 1.0
  */
+@Data
 public class UserTableDTO {
 
-    /**
-     * id主键
-     */
-    private Integer id;
+    private List<@Valid Save> save;
 
-    /**
-     * 用户名
-     */
-    @NotEmpty(message = "用户名不能为空")
-    private String userName;
+    @Data
+    public static class Save{
+        /**
+         * id主键
+         */
+        private Integer id;
 
-    /**
-     * 密码
-     */
-    @NotEmpty(message = "密码不能为空")
-    private String passWord;
+        /**
+         * 用户名
+         */
+        @NotEmpty(message = "用户名不能为空")
+        private String userName;
 
-    /**
-     * 电话
-     */
-    private String phone;
+        /**
+         * 密码
+         */
+        @NotEmpty(message = "密码不能为空")
+        private String passWord;
 
-    /**
-     * 用户真实姓名
-     */
-    @NotEmpty(message = "用户真实姓名不能为空")
-    private String realName;
+        /**
+         * 电话
+         */
+        private String phone;
 
-    /**
-     * 身份证号码
-     */
-    @NotEmpty(message = "身份证号码不能为空")
-    private String identityCard;
+        /**
+         * 用户真实姓名
+         */
+        @NotEmpty(message = "用户真实姓名不能为空")
+        private String realName;
 
-    /**
-     * 地址
-     */
-    private String address;
+        /**
+         * 身份证号码
+         */
+        @NotEmpty(message = "身份证号码不能为空")
+        private String identityCard;
 
-    /**
-     * 年纪
-     */
-    @NotNull(message = "年纪不能为空")
-    private Integer age;
+        /**
+         * 地址
+         */
+        private String address;
 
-    /**
-     * 性别1男2女
-     */
-    @NotNull(message = "性别不能为空")
-    private Integer sex;
+        /**
+         * 年纪
+         */
+        @NotNull(message = "年纪不能为空")
+        private Integer age;
 
-    public Integer getId() {
-        return id;
+        /**
+         * 性别1男2女
+         */
+        @NotNull(message = "性别不能为空")
+        private Integer sex;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
-
-    public String getIdentityCard() {
-        return identityCard;
-    }
-
-    public void setIdentityCard(String identityCard) {
-        this.identityCard = identityCard;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
 }
